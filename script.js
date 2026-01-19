944e062e698d39bb2b7beb541884012f16ac5fe6
bhaskaranb0907 <43184077+bhaskaranb0907@users.noreply.github.com>
2026-01-19 17:43:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// let score = 10;
+// score = score + 5;
+// console.log(score)
+
+
+// let smtg
+// console.log(smtg)
+
+// smtg = "hello"
+// console.log(smtg)
+
+
+// let personName = "joe";
+// // let personName = "guan"
+// let age = 18;
+// let statuss = "noob";
+// const favLanguage = "python"
+
+
+// console.log(personName,age,statuss, favLanguage)
+
+
+// let name = "Maurita";
+// let age = 18;
+// let isStudent = true;
+// const favoriteLanguage = "JavaScript";
+
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+// console.log(favoriteLanguage);
+
+// age = 19;
+// console.log(age);
+
+// favoriteLanguage = "Python"; 
+
+
+let person = {
+  name: "Maurita",
+  age: 17
+};
+
+let personCopy = person;
+
+
+personCopy.age = 18;
+
+
+console.log(person);     
+console.log(personCopy); 
+
+
+let fruits = ["apple","mango","guava"]
+console.log(fruits)
+
+let fruitcopy = fruits
+console.log(fruitcopy)
+
+fruitcopy = ["cat","dog"];
+console.log(fruitcopy)
